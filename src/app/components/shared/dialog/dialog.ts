97acc8d625e04c82f469ffbc82b33af60306554d
Nonpawit P. <nonpawit.p@outlook.com>
2025-09-11 14:35:03 +0700

    feat: add modal animation

--- src/app/components/shared/dialog/dialog.ts
+++ src/app/components/shared/dialog/dialog.ts
@@ -1,4 +1,5 @@
-import { Component, output} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, output, ViewChild} from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-dialog',
@@ -6,10 +7,19 @@ import { Component, output} from '@angular/core';
   templateUrl: './dialog.html',
   styleUrl: './dialog.css'
 })
-export class Dialog {
+export class Dialog implements AfterViewInit {
   cancelClicked = output();
 
+  @ViewChild('modalBox', { static: true }) modalBox!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit() {
+    gsap.fromTo(this.modalBox.nativeElement,
+      { scale: 0.5 },
+      { scale: 1, duration: 0.6, ease: 'elastic.out(1, 0.5)' }
+    );
+  }
+
   onCancelClick() {
-    this.cancelClicked.emit();
+    this.cancelClicked.emit()
   }
 }
